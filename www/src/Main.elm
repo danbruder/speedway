5ce6b30025ce59e40a775b75032d073ed7c9568f
dan <ddbruder@gmail.com>
2018-08-16 21:13:01 +0000

    car drives around

--- www/src/Main.elm
+++ www/src/Main.elm
@@ -4,7 +4,9 @@ import Css exposing (..)
 import Html
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (css, href, src)
+import AnimationFrame
 import Time exposing (Time, second)
+import Keyboard
 
 
 ---- MODEL ----
@@ -13,13 +15,15 @@ import Time exposing (Time, second)
 type alias Model =
     { car : Car
     , time : Time
+    , pedal : Pedal
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { car = Car Red ( 100, 100 ) 34 10
+    ( { car = Car Red ( 0, 0 ) 0 0
       , time = 0
+      , pedal = PedalNotPressed
       }
     , Cmd.none
     )
@@ -31,40 +35,53 @@ init =
 
 type Msg
     = Tick Time
-    | KeyDown Int
+    | KeyDownMsg Keyboard.KeyCode
+    | KeyUpMsg Keyboard.KeyCode
     | NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        KeyDown key ->
-            ( model, Cmd.none )
+        KeyDownMsg code ->
+            case code |> controlFromCode of
+                Right ->
+                    let
+                        newModel =
+                            model.car.rotation
+                                + rotationSpeed
+                                |> asRotationIn model.car
+                                |> asCarIn model
+                    in
+                        ( newModel, Cmd.none )
+
+                Left ->
+                    let
+                        newModel =
+                            model.car.rotation
+                                - rotationSpeed
+                                |> asRotationIn model.car
+                                |> asCarIn model
+                    in
+                        ( newModel, Cmd.none )
+
+                Space ->
+                    ( { model | pedal = PedalPressed }, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        KeyUpMsg code ->
+            case code |> controlFromCode of
+                Space ->
+                    ( { model | pedal = PedalNotPressed }, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
 
         Tick newTime ->
-            let
-                ( currentX, currentY ) =
-                    model.car.location
-
-                newX =
-                    currentX + model.car.speed * cos (model.car.rotation)
-
-                newY =
-                    currentY + model.car.speed * sin (model.car.rotation)
-
-                newPos =
-                    ( newX, newY )
-
-                newRotation =
-                    model.car.rotation
-
-                { car } =
-                    model
-
-                newCar =
-                    { car | location = newPos, rotation = newRotation }
-            in
-                ( { model | car = newCar }, Cmd.none )
+            model
+                |> updateFromTick msg
 
         NoOp ->
             ( model, Cmd.none )
@@ -83,18 +100,18 @@ view model =
         rotation =
             model.car.rotation
     in
-        div [ onKeyDown KeyDown ]
-            [ div
-                [ css
-                    [ width (px 100)
-                    , height (px 200)
-                    , backgroundColor (rgb 123 123 123)
-                    , transforms
+        div []
+            [ img
+                [ src "http://clipartriver.com/wp-content/uploads/2017/11/top-view-car-clipart-images-school-pics-free-000.png"
+                , css
+                    [ transforms
                         [ translate3d (px x) (px y) (px 0)
                         , rotateZ (deg rotation)
                         ]
                     ]
                 ]
+                []
+            , div []
                 []
             ]
 
@@ -105,7 +122,11 @@ view model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every second Tick
+    Sub.batch
+        [ AnimationFrame.diffs Tick
+        , Keyboard.downs KeyDownMsg
+        , Keyboard.ups KeyUpMsg
+        ]
 
 
 
@@ -150,3 +171,147 @@ type alias Rotation =
 
 type alias Speed =
     Float
+
+
+type Pedal
+    = PedalPressed
+    | PedalNotPressed
+
+
+type Controls
+    = Left
+    | Right
+    | Space
+    | None
+
+
+controlFromCode : Keyboard.KeyCode -> Controls
+controlFromCode code =
+    case code of
+        37 ->
+            Left
+
+        39 ->
+            Right
+
+        32 ->
+            Space
+
+        _ ->
+            None
+
+
+setRotation : Rotation -> Car -> Car
+setRotation rotation car =
+    { car | rotation = rotation }
+
+
+asRotationIn : Car -> Rotation -> Car
+asRotationIn =
+    flip setRotation
+
+
+setSpeed : Speed -> Car -> Car
+setSpeed speed car =
+    { car | speed = speed }
+
+
+asSpeedIn : Car -> Speed -> Car
+asSpeedIn =
+    flip setSpeed
+
+
+setCar : Car -> Model -> Model
+setCar car model =
+    { model | car = car }
+
+
+asCarIn : Model -> Car -> Model
+asCarIn =
+    flip setCar
+
+
+rotationSpeed : Float
+rotationSpeed =
+    8
+
+
+maxSpeed : Float
+maxSpeed =
+    5
+
+
+updateFromTick : Msg -> Model -> ( Model, Cmd Msg )
+updateFromTick msg model =
+    let
+        newModel =
+            model
+                |> updateSpeed
+                |> updateLocation
+    in
+        ( newModel, Cmd.none )
+
+
+updateSpeed : Model -> Model
+updateSpeed model =
+    case model.pedal of
+        PedalPressed ->
+            let
+                newSpeed =
+                    if model.car.speed < maxSpeed then
+                        model.car.speed + 1
+                    else
+                        model.car.speed
+
+                newModel =
+                    newSpeed
+                        |> asSpeedIn model.car
+                        |> asCarIn model
+            in
+                newModel
+
+        PedalNotPressed ->
+            let
+                car =
+                    model.car
+
+                speed =
+                    car.speed
+
+                newSpeed =
+                    if speed > 0 then
+                        speed - 0.7
+                    else
+                        0
+            in
+                { model | car = { car | speed = newSpeed } }
+
+
+updateLocation : Model -> Model
+updateLocation model =
+    let
+        ( currentX, currentY ) =
+            model.car.location
+
+        speed =
+            model.car.speed
+
+        angle =
+            model.car.rotation * pi / 180
+
+        newX =
+            currentX + speed * (angle |> cos)
+
+        newY =
+            currentY + speed * (angle |> sin)
+
+        newPos =
+            ( newX, newY )
+
+        { car } =
+            model
+
+        newCar =
+            { car | location = newPos }
+    in
+        { model | car = newCar }
